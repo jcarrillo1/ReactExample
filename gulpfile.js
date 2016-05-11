b11b764f1c60f62ef658b7a6847d7d87524469bc
jcarrillo1 <xjcarrillox@gmail.com>
2016-05-10 22:34:09 -0400

    Initial update 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 "use strict";
 var gulp = require('gulp'),
     connect = require('gulp-connect'), //Runs local dev server
-    open = require('gulp-open'); //Opens a URL in the web browser
-
+    open = require('gulp-open'), //Opens a URL in the web browser
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream');
+    
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
+        // Looks for these files
         html: './src/*.html',
+        js: './src/**/*.js',
+        mainJs: './src/main.js', 
         dist: './dist'
     }
 }
@@ -35,8 +41,19 @@ gulp.task('html', function() {
         .pipe(connect.reload()); //Connect reloads anytimes there is a change
 });
 
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()  //put it all in one js file
+        .on('error', console.error.bind(console)) //show any errors on console
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+})
+
 gulp.task('watch', function() {
-   gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });    
     
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
